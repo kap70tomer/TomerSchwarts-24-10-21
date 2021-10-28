1e209ec7ec80fa8f5c08180da897923056ed9b31
‪Tomer Schwartz‬‏ <65711940+kap70tomer@users.noreply.github.com>
2021-10-28 20:39:08 +0300

    added static BG state

--- src/components/BackGround/ResponsiveGB.js
+++ src/components/BackGround/ResponsiveGB.js
@@ -6,11 +6,15 @@ import { useSelector } from 'react-redux';
 const ResponsiveGBComponent = () => {
     const currentWeather = useSelector(state => state.weather.current);
     const isDarkMode = useSelector(state => state.isDark);
-    const BGImage = `linear-gradient(rgba(160, 245, 255 , 25%), ${isDarkMode ?
-        '#000718' : '#fff'}), url(${currentWeather.Photos[0].LandscapeLink.replace("_L_L", "_L_XXL")})`;
-
+    const BGImage = 'linear-gradient(rgba(160, 245, 255 , 25%)';
+    
+    if(currentWeather){
+        const BGImage = `linear-gradient(rgba(160, 245, 255 , 25%), ${isDarkMode ?
+            '#000718' : '#fff'}), url(${currentWeather.Photos[0].LandscapeLink.replace("_L_L", "_L_XXL")})`;
+        }
+            
     return (
-        <div className="responsiveBG" style={{ backgroundImage: BGImage }} ></div>
+        <div className="responsiveBG" style={{ backgroundImage: BGImage}} ></div>
     );
 };
 export default ResponsiveGBComponent;
